0990223f808ca45e5707b8de57aefc9d89756a4f
brenetic <brendan.butler@digital.justice.gov.uk>
2020-09-09 11:50:55 +0100

    Generate basic page steps flow html

--- generate_static_site.rb
+++ generate_static_site.rb
@@ -11,25 +11,47 @@ class Metatron
         )
       ))
     end
+    @html_pages = []
   end
 
   def html
-    @pages.map do |page|
-      if page['_type'] == 'page.start'
-        index_template = File.read(
-          File.join(File.dirname(__FILE__), 'views/index.erb')
-        )
-        renderer = ERB.new(index_template)
-        heading = page['heading']
-        lede = page['lede']
-        steps = page['steps']
-        next_page = steps.first
+    @steps = start_page['steps']
+    @html_pages << start_page_html
 
-        next_url = @pages.find { |page| page['_id'] == next_page }['url']
+    @steps.each_with_index do |step, index|
+      page = @pages.find { |page| page['_id'] == step }
 
-        { "#{page['_id']}.html" => renderer.result(binding) }
+      template = File.read(
+        File.join(File.dirname(__FILE__), "views/#{page['url']}.erb")
+      )
+      renderer = ERB.new(template)
+
+      heading = page['heading']
+      unless page['_type'] == 'page.confirmation'
+        next_url = @pages.find { |page| page['_id'] == @steps[index + 1] }['url']
       end
+
+      @html_pages << { "#{page['_id']}.html" => renderer.result(binding) }
     end
+    @html_pages
+  end
+
+  def start_page
+    @_start_page ||= @pages.find { |page| page['_type'] == 'page.start' }
+  end
+
+  def start_page_html
+    index_template = File.read(
+      File.join(File.dirname(__FILE__), 'views/index.erb')
+    )
+    renderer = ERB.new(index_template)
+    heading = start_page['heading']
+    lede = start_page['lede']
+    next_page = @steps.first
+
+    next_url = @pages.find { |page| page['_id'] == next_page }['url']
+
+    { "#{start_page['_id']}.html" => renderer.result(binding) }
   end
 end
 
