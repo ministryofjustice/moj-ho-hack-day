9ccea31c444dd7c850660254915862c96c664ecf
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-09-09 11:14:50 +0100

    Talk to the hand

--- generate_static_site.rb
+++ generate_static_site.rb
@@ -1,21 +1,36 @@
-start_page = JSON.parse(
-  File.read(
-    File.join(
-      File.dirname(__FILE__),
-      'metadata/page/page.start.json'
-    )
-  )
-)
+require 'json'
+require 'erb'
 
-steps = start_page['steps']
-next_page = steps.first
+class Metatron
+  def initialize
+    @pages = Dir['metadata/page/*.json'].map do |file|
+      JSON.parse(File.read(
+        File.join(
+          File.dirname(__FILE__),
+          file
+        )
+      ))
+    end
+  end
 
-next_json = JSON.parse(
-  File.read(
-    File.join(
-      File.dirname(__FILE__),
-        "metadata/page/#{next_page}.json"
-    )
-  )
-)
-next_url = next_json['url']
+  def html
+    @pages.map do |page|
+      if page['_type'] == 'page.start'
+        index_template = File.read(
+          File.join(File.dirname(__FILE__), 'views/index.erb')
+        )
+        renderer = ERB.new(index_template)
+        heading = page['heading']
+        lede = page['lede']
+        steps = page['steps']
+        next_page = steps.first
+
+        next_url = @pages.find { |page| page['_id'] == next_page }['url']
+
+        { "#{page['_id']}.html" => renderer.result(binding) }
+      end
+    end
+  end
+end
+
+puts Metatron.new.html
